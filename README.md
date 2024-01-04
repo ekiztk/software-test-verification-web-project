7b8aceb9b8e6dda720b0708dc699195ca80a06b0
ekiztk <ekiztk@gmail.com>
2024-01-04 17:39:18 +0300

    last update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 run server: python manage.py runserver
 
+test: python manage.py test
+
 activate environment: stvvenv\Scripts\activate
 
 admin
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,16 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 class ImageUploadModel(models.Model):
     image = models.ImageField(upload_to='images/')
+
+    # test 11 if image smaller than 5mb
+    def clean(self):
+        img = self.image
+        if img:
+            if img.size > 5*1024*1024:  # 5MB limit
+                raise ValidationError("Image file too large ( > 5mb )")
+        else:
+            raise ValidationError("Couldn't read uploaded image")
